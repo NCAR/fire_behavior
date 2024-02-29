26d622227aa24be7a9d76d9f818e0e836aa78f80
Pedro A. Jimenez <jimenez@ucar.edu>
2024-02-28 20:46:14 -0700

    Code to ignite prescribed fires in a subroutine

--- physics/fire_model_mod.F90
+++ physics/fire_model_mod.F90
@@ -31,10 +31,7 @@
       character (len = 128) :: msg
       integer :: stat_lev = 1
       real :: start_time_ig, end_time_ig, time_start
-      real, parameter :: EPSILON = 0.00001
       integer :: ignitions_done
-        ! number of gridpts ignited in a given ignition
-      integer :: ignited_tile(config_flags%fire_num_ignitions)
 
 
       ifds = grid%ifds
@@ -79,6 +76,44 @@
       end do
 
         ! Check for ignitions
+      call Ignite_prescribed_fires (grid, config_flags, time_start, ifts, ifte, jfts, jfte, ifms, ifme, jfms, jfme, ifds, ifde, jfds, jfde)
+
+        ! compute the heat fluxes from the fuel burned
+        ! needs lfn and tign from neighbors so halo must be updated before
+      call fuel_left (ifms, ifme, jfms, jfme, ifts, ifte, jfts, jfte, ifts, ifte, jfts, jfte, &
+          grid%lfn,grid%tign_g,grid%fuel_time, time_start + grid%dt, fuel_frac_end, grid%fire_area, &
+          config_flags%fire_print_msg)
+
+      do j = jfts, jfte
+        do i = ifts, ifte
+          fuel_frac_burnt(i, j) = grid%fuel_frac(i, j) - fuel_frac_end(i, j) ! fuel lost this timestep
+          grid%burnt_area_dt(i, j) = fuel_frac_burnt(i, j)
+          grid%fuel_frac(i, j) = fuel_frac_end(i, j) ! copy new value to state array
+        end do
+      end do
+
+      call Calc_fire_fluxes (grid%dt, grid, ifms, ifme, jfms, jfme, ifts, ifte, jfts, jfte, &
+          ifts, ifte, jfts, jfte, grid%fuel_load_g, fuel_frac_burnt, grid%fgrnhfx, grid%fgrnqfx)
+
+      call Calc_smoke_emissions (grid, config_flags, ifts, ifte, jfts, jfte)
+
+    end subroutine Advance_fire_model
+
+    subroutine Ignite_prescribed_fires (grid, config_flags, time_start, ifts, ifte, jfts, jfte, ifms, ifme, jfms, jfme, ifds, ifde, jfds, jfde)
+
+      implicit none
+
+      type (namelist_t), intent (in) :: config_flags
+      type (state_fire_t), intent (in out) :: grid
+      real, intent (in) :: time_start
+      integer, intent (in) :: ifts, ifte, jfts, jfte, ifms, ifme, jfms, jfme, ifds, ifde, jfds, jfde
+
+      real, parameter :: EPSILON = 0.00001
+      integer :: i, j, ig, ignitions_done, start_time_ig, end_time_ig, ignited
+        ! number of gridpts ignited in a given ignition
+      integer :: ignited_tile(config_flags%fire_num_ignitions)
+
+
       ig = 1
       start_time_ig = grid%ignition_lines%start_time(ig)
       end_time_ig  = grid%ignition_lines%end_time(ig)
@@ -101,36 +136,14 @@
 
       else if (.not. config_flags%fire_is_real_perim) then
         do ig = 1, config_flags%fire_num_ignitions
-            !  for now, check for ignition every time step...
-            !        if(ignition_line(ig)%end_time>=time_start.and.ignition_line(ig)%start_time<time_start+dt)then 
           call grid%ignition_lines%Ignite_fire (ifms, ifme, jfms, jfme, ifts, ifte, jfts, jfte, &
               ig, time_start, time_start + grid%dt,  grid%lons, grid%lats, grid%unit_fxlong, grid%unit_fxlat, &
               grid%lfn, grid%tign_g, ignited)
           ignitions_done = ignitions_done + 1
           ignited_tile(ignitions_done) = ignited
-            !        endif
         end do
       end if
-           
-        ! compute the heat fluxes from the fuel burned
-        ! needs lfn and tign from neighbors so halo must be updated before
-      call fuel_left (ifms, ifme, jfms, jfme, ifts, ifte, jfts, jfte, ifts, ifte, jfts, jfte, &
-          grid%lfn,grid%tign_g,grid%fuel_time, time_start + grid%dt, fuel_frac_end, grid%fire_area, &
-          config_flags%fire_print_msg)
 
-      do j = jfts, jfte
-        do i = ifts, ifte
-          fuel_frac_burnt(i, j) = grid%fuel_frac(i, j) - fuel_frac_end(i, j) ! fuel lost this timestep
-          grid%burnt_area_dt(i, j) = fuel_frac_burnt(i, j)
-          grid%fuel_frac(i, j) = fuel_frac_end(i, j) ! copy new value to state array
-        end do
-      end do
-
-      call Calc_fire_fluxes (grid%dt, grid, ifms, ifme, jfms, jfme, ifts, ifte, jfts, jfte, &
-          ifts, ifte, jfts, jfte, grid%fuel_load_g, fuel_frac_burnt, grid%fgrnhfx, grid%fgrnqfx)
-
-      call Calc_smoke_emissions (grid, config_flags, ifts, ifte, jfts, jfte)
-
-    end subroutine Advance_fire_model
+    end subroutine Ignite_prescribed_fires
 
   end module fire_model_mod
